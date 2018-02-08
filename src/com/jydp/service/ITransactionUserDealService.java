10d1983a34f489462d65550bc874b2e3eff7dd29
fukang <729761220@qq.com>
2018-02-08 12:00:27 +0800

    做单记录dao层修改和成交记录接口

--- src/com/jydp/service/ITransactionUserDealService.java
+++ src/com/jydp/service/ITransactionUserDealService.java
@@ -2,6 +2,7 @@ package com.jydp.service;
 
 import com.jydp.entity.DO.transaction.TransactionUserDealDO;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -18,4 +19,49 @@ public interface ITransactionUserDealService {
      */
     List<TransactionUserDealDO> getTransactionUserDeallist(int userId);
 
+    /**
+     * 新增成交记录
+     * @param orderNo  记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param pendingOrderNo  挂单记录号
+     * @param userId  用户Id
+     * @param paymentType  收支类型,1：买入，2：卖出
+     * @param currencyId  币种Id
+     * @param currencyName  货币名称
+     * @param currencyNumber  成交数量
+     * @param currencyTotalPrice  成交总价
+     * @param remark  备注
+     * @param addTime  添加时间
+     * @return  操作成功：返回true，操作失败：返回false
+     */
+    boolean insertTransactionUserDeal(String orderNo, String pendingOrderNo, int userId, int paymentType,
+                                      int currencyId, String currencyName, double currencyNumber, double currencyTotalPrice,
+                                      String remark, Timestamp addTime);
+
+    /**
+     * 查询成交记录条数(后台)
+     * @param orderNo  记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param userAccount  用户账号
+     * @param paymentType  收支类型,1：买入，2：卖出
+     * @param currencyName  货币名称
+     * @param startAddTime  起始完成时间
+     * @param endAddTime  结束完成时间
+     * @return  操作成功：返回成交记录条数，操作失败：返回0
+     */
+    int countTransactionUserDealForBack(String orderNo, String userAccount, int paymentType, String currencyName,
+                                        Timestamp startAddTime, Timestamp endAddTime);
+
+    /**
+     * 查询成交记录(后台)
+     * @param orderNo  记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param userAccount  用户账号
+     * @param paymentType  收支类型,1：买入，2：卖出
+     * @param currencyName  货币名称
+     * @param startAddTime  起始完成时间
+     * @param endAddTime  结束完成时间
+     * @param pageNumber  当前页数
+     * @param pageSize  每页条数
+     * @return  操作成功：返回成交记录，操作失败：返回null
+     */
+    List<TransactionUserDealDO> listTransactionUserDealForBack(String orderNo, String userAccount, int paymentType, String currencyName,
+                                                               Timestamp startAddTime, Timestamp endAddTime, int pageNumber, int pageSize);
 }
--- src/com/jydp/service/impl/transaction/TransactionUserDealServiceImpl.java
+++ src/com/jydp/service/impl/transaction/TransactionUserDealServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -31,4 +32,69 @@ public class TransactionUserDealServiceImpl implements ITransactionUserDealServi
     public List<TransactionUserDealDO> getTransactionUserDeallist(int userId) {
         return transactionUserDealDao.getTransactionUserDeallist(userId);
     }
+
+    /**
+     * 新增成交记录
+     * @param orderNo  记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param pendingOrderNo  挂单记录号
+     * @param userId  用户Id
+     * @param paymentType  收支类型,1：买入，2：卖出
+     * @param currencyId  币种Id
+     * @param currencyName  货币名称
+     * @param currencyNumber  成交数量
+     * @param currencyTotalPrice  成交总价
+     * @param remark  备注
+     * @param addTime  添加时间
+     * @return  操作成功：返回true，操作失败：返回false
+     */
+    public boolean insertTransactionUserDeal(String orderNo, String pendingOrderNo, int userId, int paymentType,
+                                      int currencyId, String currencyName, double currencyNumber, double currencyTotalPrice,
+                                      String remark, Timestamp addTime){
+        TransactionUserDealDO transactionUserDealDO = new TransactionUserDealDO();
+        transactionUserDealDO.setOrderNo(orderNo);
+        transactionUserDealDO.setPendingOrderNo(pendingOrderNo);
+        transactionUserDealDO.setUserId(userId);
+        transactionUserDealDO.setPaymentType(paymentType);
+        transactionUserDealDO.setCurrencyId(currencyId);
+        transactionUserDealDO.setCurrencyName(currencyName);
+        transactionUserDealDO.setCurrencyNumber(currencyNumber);
+        transactionUserDealDO.setCurrencyTotalPrice(currencyTotalPrice);
+        transactionUserDealDO.setRemark(remark);
+        transactionUserDealDO.setAddTime(addTime);
+
+        return transactionUserDealDao.insertTransactionUserDeal(transactionUserDealDO);
+    }
+
+    /**
+     * 查询成交记录条数(后台)
+     * @param orderNo  记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param userAccount  用户账号
+     * @param paymentType  收支类型,1：买入，2：卖出
+     * @param currencyName  货币名称
+     * @param startAddTime  起始完成时间
+     * @param endAddTime  结束完成时间
+     * @return  操作成功：返回成交记录条数，操作失败：返回0
+     */
+    public int countTransactionUserDealForBack(String orderNo, String userAccount, int paymentType, String currencyName,
+                                        Timestamp startAddTime, Timestamp endAddTime){
+        return transactionUserDealDao.countTransactionUserDealForBack(orderNo, userAccount, paymentType, currencyName, startAddTime, endAddTime);
+    }
+
+    /**
+     * 查询成交记录(后台)
+     * @param orderNo  记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param userAccount  用户账号
+     * @param paymentType  收支类型,1：买入，2：卖出
+     * @param currencyName  货币名称
+     * @param startAddTime  起始完成时间
+     * @param endAddTime  结束完成时间
+     * @param pageNumber  当前页数
+     * @param pageSize  每页条数
+     * @return  操作成功：返回成交记录，操作失败：返回null
+     */
+    public List<TransactionUserDealDO> listTransactionUserDealForBack(String orderNo, String userAccount, int paymentType, String currencyName,
+                                                               Timestamp startAddTime, Timestamp endAddTime, int pageNumber, int pageSize){
+        return transactionUserDealDao.listTransactionUserDealForBack(orderNo, userAccount, paymentType, currencyName, startAddTime, endAddTime, pageNumber, pageSize);
+    }
+
 }
