2c1dc2cf0280024c2179f0ef50b70a7e65c88561
huangzhe <huangzhe1013@foxmail.com>
2018-02-28 15:52:32 +0800

    添加修改匹配交易状态接口

--- src/com/jydp/dao/ITransactionPendOrderDao.java
+++ src/com/jydp/dao/ITransactionPendOrderDao.java
@@ -120,4 +120,31 @@ public interface ITransactionPendOrderDao {
      */
     boolean updateAllRevoke(String pendingOrderNo, double revokeNumber, Timestamp endTime);
 
+    /**
+     * 查询最近的一笔正在挂单的挂单记录（仅用于匹配交易）
+     * @param userId 用户Id（不根据userId时填0）
+     * @param currencyId 币种Id
+     * @param paymentType 交易类型,1：买入，2：卖出
+     * @return 操作成功：返回挂单记录，操作失败：返回null
+     */
+    TransactionPendOrderDO getLastTransactionPendOrder(int userId, int currencyId, int paymentType);
+
+    /**
+     * 修改挂单状态为部分成交（仅用于匹配交易）
+     * @param pendingOrderNo 记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param dealNumber 成交数量
+     * @param endTime 完成时间
+     * @return 操作成功：返回true，操作失败：返回false
+     */
+    boolean updatePartDeal(String pendingOrderNo, double dealNumber, Timestamp endTime);
+
+    /**
+     * 修改挂单状态为全部成交（仅用于匹配交易）
+     * @param pendingOrderNo 记录号,业务类型（2）+日期（6）+随机位（10）
+     * @param dealNumber 成交数量
+     * @param endTime 完成时间
+     * @return 操作成功：返回true，操作失败：返回false
+     */
+    boolean updateAllDeal(String pendingOrderNo, double dealNumber, Timestamp endTime);
+
 }
