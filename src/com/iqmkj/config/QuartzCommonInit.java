4ce4ec4507c183ba7c64c9fb9eb459c2e50f7ecc
fukang <729761220@qq.com>
2018-03-06 10:44:31 +0800

    定时器初始化类

---
+++ src/com/iqmkj/config/QuartzCommonInit.java
@@ -0,0 +1,42 @@
+package com.iqmkj.config;
+
+import com.jydp.entity.DO.transaction.TransactionCurrencyDO;
+import com.jydp.entity.DO.transaction.TransactionDealRedisDO;
+import com.jydp.entity.VO.TransactionCurrencyVO;
+import com.jydp.service.IRedisService;
+import com.jydp.service.ITransactionCurrencyService;
+import com.jydp.service.ITransactionDealRedisService;
+import com.jydp.service.ITransactionRedisDealCommonService;
+import config.RedisKeyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * 定时器初始化
+ * @author fk
+ */
+public class QuartzCommonInit {
+
+    /** redis成交记录 */
+    @Autowired
+    private ITransactionDealRedisService transactionDealRedisService;
+
+    /** 交易币种 */
+    @Autowired
+    private ITransactionCurrencyService transactionCurrencyService;
+
+    /** redis服务 */
+    @Autowired
+    private IRedisService redisService;
+
+    /** 定时器公共服务 */
+    @Autowired
+    private ITransactionRedisDealCommonService transactionRedisDealCommonService;
+
+    /** 执行初始化 */
+    public void executeInit() {
+        transactionRedisDealCommonService.userDealForRedis();
+    }
+
+}
