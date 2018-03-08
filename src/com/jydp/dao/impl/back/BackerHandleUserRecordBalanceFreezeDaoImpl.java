1922c5489440a7c8a3a255f2f08da2ca250c56bc
sy <sy@163.com>
2018-03-09 02:24:48 +0800

    后台管理员增减用户

--- src/com/jydp/dao/impl/back/BackerHandleUserRecordBalanceFreezeDaoImpl.java
+++ src/com/jydp/dao/impl/back/BackerHandleUserRecordBalanceFreezeDaoImpl.java
@@ -7,6 +7,11 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 后台管理员增减用户冻结余额记录
  * @author sy
@@ -36,5 +41,66 @@ public class BackerHandleUserRecordBalanceFreezeDaoImpl implements IBackerHandle
         } else {
             return false;
         }
+    }
+
+    /**
+     * 根据筛选条件获取后台增减用户余额的记录
+     * @param userAccount  用户账号
+     * @param typeHandle  操作类型
+     * @param backerAccount  操作管理员账号
+     * @param startAddTime  开始添加时间
+     * @param endAddTime  结束添加时间
+     * @param pageNumber 查询的页码
+     * @param pageSize 每页的信息数量
+     * @return  操作成功：返回记录集合，操作失败：返回null
+     */
+    public List<BackerHandleUserRecordBalanceFreezeDO> getUserRecordBalanceFreezeList(String userAccount, int typeHandle, String backerAccount,
+                                                                          Timestamp startAddTime, Timestamp endAddTime, int pageNumber, int pageSize){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("userAccount", userAccount);
+        map.put("typeHandle", typeHandle);
+        map.put("backerAccount", backerAccount);
+        map.put("startAddTime", startAddTime);
+        map.put("endAddTime", endAddTime);
+        map.put("startNumber", pageSize*pageNumber);
+        map.put("pageSize", pageSize);
+        List<BackerHandleUserRecordBalanceFreezeDO> resultList = null;
+
+        try {
+            resultList = sqlSessionTemplate.selectList("BackerHandleUserRecordBalanceFreeze_getUserRecordBalanceFreezeList", map);
+        } catch (Exception e) {
+            LogUtil.printErrorLog(e);
+        }
+
+        return resultList;
+    }
+
+    /**
+     * 根据筛选条件获取后台增减用户余额的记录数量
+     * @param userAccount  用户账号
+     * @param typeHandle  操作类型
+     * @param backerAccount  操作管理员账号
+     * @param startAddTime  开始添加时间
+     * @param endAddTime  结束添加时间
+     * @return  操作成功：返回记录集合数量，操作失败：返回0
+     */
+    public int getUserRecordBalanceFreezeNumber(String userAccount, int typeHandle, String backerAccount,
+                                          Timestamp startAddTime, Timestamp endAddTime){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("userAccount", userAccount);
+        map.put("typeHandle", typeHandle);
+        map.put("backerAccount", backerAccount);
+        map.put("startAddTime", startAddTime);
+        map.put("endAddTime", endAddTime);
+
+        int result = 0;
+
+        try {
+            result = sqlSessionTemplate.selectOne("BackerHandleUserRecordBalanceFreeze_getUserRecordBalanceFreezeNumber", map);
+        } catch (Exception e) {
+            LogUtil.printErrorLog(e);
+        }
+
+        return result;
     }
 }
