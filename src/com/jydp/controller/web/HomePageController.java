a411e0623522844dd5af17d6b15efb21d17617fc
yangkai <664792183@qq.com>
2018-03-14 18:55:19 +0800

    修改首页特殊字符标签转换问题

--- src/com/jydp/controller/web/HomePageController.java
+++ src/com/jydp/controller/web/HomePageController.java
@@ -1,6 +1,7 @@
 package com.jydp.controller.web;
 
 import com.alibaba.fastjson.JSONObject;
+import com.iqmkj.utils.StringUtil;
 import com.jydp.entity.BO.JsonObjectBO;
 import com.jydp.entity.DO.system.SystemAdsHomepagesDO;
 import com.jydp.entity.DO.system.SystemBusinessesPartnerDO;
@@ -17,6 +18,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.HtmlUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -48,17 +51,58 @@ public class HomePageController {
         //查询首页广告列表
         List<SystemAdsHomepagesDO> systemAdsHomepagesDOList = (List<SystemAdsHomepagesDO>)redisService.getValue(RedisKeyConfig.HOMEPAGE_ADS);
 
+        if (systemAdsHomepagesDOList != null && systemAdsHomepagesDOList.size() > 0) {
+            for (SystemAdsHomepagesDO systemAdsHomepagesDO : systemAdsHomepagesDOList) {
+                String webLinkUrl = systemAdsHomepagesDO.getWebLinkUrl();
+                if (StringUtil.isNotNull(webLinkUrl)) {
+                    webLinkUrl = HtmlUtils.htmlEscape(webLinkUrl);
+                    systemAdsHomepagesDO.setWebLinkUrl(webLinkUrl);
+                }
+            }
+        }
+
+
         //查询所有币行情信息
         List<TransactionUserDealDTO> transactionUserDealDTOList = (List<TransactionUserDealDTO>)redisService.getValue(RedisKeyConfig.HOMEPAGE_CURRENCYMARKET);
 
         //查询系统公告列表
         List<SystemNoticeDO> systemNoticeDOList = (List<SystemNoticeDO>)redisService.getValue(RedisKeyConfig.HOMEPAGE_NOTICE);
 
+        if (systemNoticeDOList != null && systemNoticeDOList.size() > 0) {
+            for (SystemNoticeDO systemNotice : systemNoticeDOList) {
+                String noticeTitle = systemNotice.getNoticeTitle();
+                if (StringUtil.isNotNull(noticeTitle)) {
+                    noticeTitle = HtmlUtils.htmlEscape(noticeTitle);
+                    systemNotice.setNoticeTitle(noticeTitle);
+                }
+            }
+        }
+
         //查询热门话题列表
         List<SystemHotDO> systemHotDOList = (List<SystemHotDO>)redisService.getValue(RedisKeyConfig.HOMEPAGE_HOTTOPIC);
 
+        if (systemHotDOList != null && systemHotDOList.size() > 0) {
+            for (SystemHotDO systemHot : systemHotDOList) {
+                String noticeTitle = systemHot.getNoticeTitle();
+                if (StringUtil.isNotNull(noticeTitle)) {
+                    noticeTitle = HtmlUtils.htmlEscape(noticeTitle);
+                    systemHot.setNoticeTitle(noticeTitle);
+                }
+            }
+        }
+
         //查询合作商家列表
         List<SystemBusinessesPartnerDO> systemBusinessesPartnerDOList = (List<SystemBusinessesPartnerDO>)redisService.getValue(RedisKeyConfig.HOMEPAGE_PARTNER);
+
+        if (systemBusinessesPartnerDOList != null && systemBusinessesPartnerDOList.size() > 0) {
+            for (SystemBusinessesPartnerDO systemBusinessesPartnerDO : systemBusinessesPartnerDOList) {
+                String webLinkUrl = systemBusinessesPartnerDO.getWebLinkUrl();
+                if (StringUtil.isNotNull(webLinkUrl)) {
+                    webLinkUrl = HtmlUtils.htmlEscape(webLinkUrl);
+                    systemBusinessesPartnerDO.setWebLinkUrl(webLinkUrl);
+                }
+            }
+        }
 
         request.setAttribute("systemAdsHomepagesDOList",systemAdsHomepagesDOList);
         request.setAttribute("systemNoticeDOList",systemNoticeDOList);
