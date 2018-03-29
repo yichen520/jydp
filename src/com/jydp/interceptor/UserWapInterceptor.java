791ced47ede0e9b0462c5e847e42d1bd7b9133d8
xiaozhu001 <m15832013094@163.com>
2018-03-29 18:16:18 +0800

    去除登录记录来访地址栏地址

--- src/com/jydp/interceptor/UserWapInterceptor.java
+++ src/com/jydp/interceptor/UserWapInterceptor.java
@@ -60,20 +60,6 @@ public class UserWapInterceptor implements HandlerInterceptor {
 
         if (userSession == null) {
             //如果是ajax请求
-            String referer = request.getHeader("referer");
-            if(!StringUtil.isNotNull(referer)){
-                request.getRequestDispatcher("/wapLogin").forward(request, response);
-                return false;
-            }
-            String host = request.getHeader("Host");
-            if(!StringUtil.isNotNull(host)){
-                request.getRequestDispatcher("/wapLogin").forward(request, response);
-                return false;
-            }
-            String[] strs = referer.split(host);
-            String uriStr = strs[1];
-            uriStr = uriStr.substring(5);
-            request.getSession().setAttribute("uriStr", uriStr);
 
             if (request.getHeader("x-requested-with") != null
                     && request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {
