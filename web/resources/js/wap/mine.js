5104fbdd164b8bcd1323725d1d34fa66bcc01329
xushilong3623 <987784986@qq.com>
2018-03-30 20:33:31 +0800

    侧边栏滑动问题

--- web/resources/js/wap/mine.js
+++ web/resources/js/wap/mine.js
@@ -11,6 +11,8 @@ $(function () {
                 var myTemplate = Handlebars.compile($("#table-template").html());
                 $('#currencyList').html(myTemplate(result.transactionUserDealList));
             })
+            $('html').css('overflow','hidden','height','100%');
+            $('body').css('overflow','hidden','height','100%');
             $('.closeAnthoer').css("height", bgHeight + "px");
             $('.choseBz').css("height", bgHeight + "px");
             $('.choseBzBox').css("height", bgHeight + "px");
@@ -18,6 +20,8 @@ $(function () {
             $('.choseBzBox').animate({left: '0'}, "500");
         });
         $('.closeBox').on('click', function () {
+            $('html').css('overflow','auto');
+            $('body').css('overflow','auto');
             setTimeout(function () {
                 $('.closeAnthoer').css("height", "0");
                 $('.choseBz').css("height", "0");
