3a5866b958b137a5b9b7e35e40ea35ad64dc89e0
hanhaotian <1456023704@qq.com>
2018-04-02 15:55:03 +0800

    最低价接口config 删除无用配置

--- src/config/SljzConfig.java
+++ src/config/SljzConfig.java
@@ -10,7 +10,4 @@ public class SljzConfig {
     /** 签名秘钥 */
     public static final String SIGN_SECRET_KEY = "syjys@123456";
 
-    /** 提供当前价和保底价的币种Id */
-    public static final int CURRENCY_ID = 79;
-
 }
