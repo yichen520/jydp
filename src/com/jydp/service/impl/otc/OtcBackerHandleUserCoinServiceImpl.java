e1647e3147849012bc065024ba63f79f9540f906
yangkai <664792183@qq.com>
2018-04-09 16:54:20 +0800

    场外交易成交记录Service

---
+++ src/com/jydp/service/impl/otc/OtcBackerHandleUserCoinServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jydp.service.impl.otc;
+
+import com.jydp.service.IOtcBackerHandleUserCoinService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 后台管理员增减经销商用户可用币记录
+ * @author yk
+ */
+@Service("otcBackerHandleUserCoinService")
+public class OtcBackerHandleUserCoinServiceImpl implements IOtcBackerHandleUserCoinService{
+}
---
+++ src/com/jydp/service/impl/otc/OtcTransactionUserDealServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jydp.service.impl.otc;
+
+import com.jydp.service.IOtcTransactionUserDealService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 场外交易成交记录
+ * @author yk
+ */
+@Service("otcTransactionUserDealService")
+public class OtcTransactionUserDealServiceImpl implements IOtcTransactionUserDealService {
+}
