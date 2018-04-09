f6342077647c64af204b27b5ed78dd89e105ecfa
yangkai <664792183@qq.com>
2018-04-09 22:20:15 +0800

    添加dao层

---
+++ src/com/jydp/dao/impl/otc/OtcBackerHandleUserCoinDaoImpl.java
@@ -0,0 +1,12 @@
+package com.jydp.dao.impl.otc;
+
+import com.jydp.dao.IOtcBackerHandleUserCoinDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 后台管理员增减经销商用户可用币记录
+ * @author yk
+ */
+@Repository
+public class OtcBackerHandleUserCoinDaoImpl implements IOtcBackerHandleUserCoinDao{
+}
---
+++ src/com/jydp/dao/impl/otc/OtcTransactionUserDealDaoImpl.java
@@ -0,0 +1,12 @@
+package com.jydp.dao.impl.otc;
+
+import com.jydp.dao.IOtcTransactionUserDealDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 场外交易成交记录
+ * @author yk
+ */
+@Repository
+public class OtcTransactionUserDealDaoImpl implements IOtcTransactionUserDealDao {
+}
