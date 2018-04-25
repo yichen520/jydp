23021b52c724ef3af766a7608f2fff9687828192
njx <285247493@qq.com>
2018-04-25 15:19:23 +0800

    session

---
+++ src/com/jydp/interceptor/SessionInterceptor.java
@@ -0,0 +1,37 @@
+package com.jydp.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ *  session过滤器
+ * @author njx
+ **/
+public class SessionInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, accept, content-type");
+        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        String header = request.getHeader("origin");
+        response.setHeader("Access-Control-Allow-Origin", header);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
